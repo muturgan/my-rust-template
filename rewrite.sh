8df5195fac39a742a588dd68a9876213400b2919
Сахаров Андрей Александрович <a.a.sakharov@tinkoff.ru>
2024-11-17 23:37:56 +0300

    should works both on MacOS and Linux

--- rewrite.sh
+++ rewrite.sh
@@ -6,15 +6,29 @@ repo_data=$(curl -s https://api.github.com/repos/$origin_url_path)
 name=$(echo $repo_data | awk -F '"name": "' '{print $2}' | awk -F '"' '{print $1}')
 description=$(echo $repo_data | awk -F '"description": "' '{print $2}' | awk -F '"' '{print $1}')
 
-sed -i '' "s/my-rust-template/$name/" scripts.sh
+rewrite() {
+	if [[ "$OSTYPE" == "darwin"* ]]; then
+		# MacOS
+		sed -i '' "s/$1/$2/" "$3"
+	else
+		# Linux
+		sed -i "s/$1/$2/" "$3"
+	fi
+}
 
-sed -i '' "s/name = \"my-rust-template\"/name = \"$name\"/" Cargo.toml
-sed -i '' "s/name = \"my-rust-template\"/name = \"$name\"/" Cargo.lock
+rewrite "my-rust-template" "$name" "scripts.sh"
 
-sed -i '' "s/description = \"template for new rust projects\"/description = \"$description\"/" Cargo.toml
+rewrite "name = \"my-rust-template\"" "name = \"$name\"" "Cargo.toml"
+rewrite "name = \"my-rust-template\"" "name = \"$name\"" "Cargo.lock"
 
-sed -i '' "s/# my-rust-template/# $name/" README.md
-sed -i '' '3,7d' README.md
-sed -i '' "s/template for new rust projects/$description/" README.md
+rewrite "description = \"template for new rust projects\"" "description = \"$description\"" "Cargo.toml"
+
+rewrite "# my-rust-template" "# $name" "README.md"
+if [[ "$OSTYPE" == "darwin"* ]]; then
+	sed -i '' '3,7d' README.md
+else
+	sed -i '3,7d' README.md
+fi
+rewrite "template for new rust projects" "# $description" "README.md"
 
 rm rewrite.sh
